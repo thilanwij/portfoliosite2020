cfd16cc057b60964328bba2105ff62c3f6f30d6e
Thilan <thilanwij@yahoo.com>
2020-01-29 23:04:06 -0800

    Updated the NAV Bar for Mobile and Desktop with FX

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
 // This is a commmmment
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animation){
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.4s ease forwards ${index / 7+0.5}s`;
+            }
+        });
+
+        //burger animation
+        // burger.classList.toggle("toggle-burger");
+        burger.classList.toggle("toggle");
+    });
+}
+
+navSlide();
